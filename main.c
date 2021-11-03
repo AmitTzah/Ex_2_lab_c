e93f87b4ba34ab5c5d529c86df0d67e032c6e0d1
AmitTzah <93420076+AmitTzah@users.noreply.github.com>
2021-11-03 14:58:24 +0200

    my_grep program for the first stage submission.

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+
+    char *current_line = NULL;
+    size_t n;
+
+    FILE *fptr= fopen(argv[2], "r");
+
+    if (fptr ==NULL)
+        return 1;
+    while(getline(&current_line, &n, fptr) != EOF){
+        if (strstr(current_line, argv[1]) != NULL)
+            printf("%s", current_line);
+    }
+    free(current_line);
+    fclose(fptr);
+
     return 0;
 }
